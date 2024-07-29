73b55d552323ad460be7bde8c4ecb705b96d5e89
redaachouhad <redachouhad@gmail.com>
2024-07-29 20:03:55 +0100

    1

--- frontend/pusher.ts
+++ frontend/pusher.ts
@@ -1,7 +1,7 @@
 import Pusher from "pusher-js";
 
 var pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY as string, {
-  cluster: "eu",
+  cluster: process.env.NEXT_PUBLIC_CLUSTER as string,
 });
 
 export default pusher;
